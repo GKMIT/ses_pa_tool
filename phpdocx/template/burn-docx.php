e3244a688cd23699c746ecf9412dd16e9dde7353
pradeep-chauhan <prdpchauhan4@gmail.com>
2015-07-31 12:18:37 +0530

    director issues

--- phpdocx/template/burn-docx.php
+++ phpdocx/template/burn-docx.php
@@ -35,76 +35,76 @@ try {
 	declarationOfDividend($docx, $report_id);
 	appointmentOfAuditors($docx, $report_id);
 	appointmentOfDirectors($docx, $report_id);
-//	directorsRemuneration($docx, $report_id);
-//	esops($docx, $report_id);
-//	relatedPartyTransaction($docx, $report_id);
-//	intercorporateLoans($docx, $report_id);
-//	schemeOfArrangement($docx, $report_id);
-//	corporateAction($docx, $report_id);
-//	issuesOfShares($docx, $report_id);
-//	alterrationInMoaAoa($docx, $report_id);
-//	fillInvestmentLimits($docx, $report_id);
-//	delistingOfShares($docx, $report_id);
-//	donationToCharitableTrust($docx, $report_id);
-//	officeOfProfit($docx, $report_id);
-//	disclaimerPage($docx);
-//	$com_name = getName($report_id);
-//	$compnay_name=$com_name['company_name'];
-//	$meeting_type=$com_name['meeting_type'];
-//	$meeting_date=$com_name['meeting_date'];
-//	$db_date = $meeting_date;
-//	$formated_day = date_format(date_create_from_format('Y-m-d', $db_date), 'd-m-Y');
-//	$docx->createDocx('try');
-//	$docx = new DocxUtilities();
-//	$source = 'try.docx';
-//	$target = 'try2.docx';
-//	$docx->watermarkDocx($source, $target, $type = 'image', $options = array('image' => 'bg.png', 'height' => 900, 'width' => 780, 'decolorate' => false));
-////$docx->watermarkDocx($source, $target, $type = 'image', $options = array('image' => 'bg.png','height'=>900,'width'=>500,'decolorate'=>false));
-//	require_once '../classes/MultiMerge.inc';
-//	$merge = new MultiMerge();
-//	$merge->mergeDocx('index_page.docx', array('try2.docx'), "$compnay_name"."_SES Proxy Advisory Report_"."$meeting_type"."_"."$formated_day".".docx", array());
-//	burnExcel($report_id);
-//	$zip_files_array = array(
-//		'AuditorsRemuneration.docx',
-//		'AverageCommission.docx',
-//		'BoardComposition.docx',
-//		'CSRContribution.docx',
-//		'DividendAndEarning.docx',
-//		'DividendPayoutRatio.docx',
-//		'EDRemuneration.docx',
-//		'ExecutiveCompensation.docx',
-//		'ExecutiveRemuneration.docx',
-//		'MasterExcelFile.xlsx',
-//		'PromoterShareholding.docx',
-//		'RemunerationComponents.docx',
-//		'RetireByRotation.docx',
-//		'ShareholdingPattern.docx',
-//		'StockPerformance.docx',
-//		'StockPrice.docx',
-//		'TotalCommission.docx',
-//		'UtilizationBorrowingLimits.docx',
-//		'VariationInDirectorsRemuneration.docx',
-//		"$compnay_name"."_SES Proxy Advisory Report_"."$meeting_type"."_"."$formated_day".".docx",
-//		'graph_excel.xlsx'
-//	);
-//	$zip = new ZipArchive;
-//	if ($zip->open("$compnay_name.zip", ZipArchive::CREATE)) {
-//		foreach ($zip_files_array as $zip_file_name) {
-//			$zip->addFile($zip_file_name);
-//		}
-//	}
-//	$zip->close();
-//	$zip_file = "$compnay_name.zip";
-//	header('Content-type: application/zip');
-//	header('Content-Disposition: attachment; filename="' . basename($zip_file) . '"');
-//	header("Content-length: " . filesize($zip_file));
-//	header("Pragma: no-cache");
-//	header("Expires: 0");
-//	ob_clean();
-//	flush();
-//	readfile($zip_file);
-//	unlink($zip_file);
-//	exit;
+	directorsRemuneration($docx, $report_id);
+	esops($docx, $report_id);
+	relatedPartyTransaction($docx, $report_id);
+	intercorporateLoans($docx, $report_id);
+	schemeOfArrangement($docx, $report_id);
+	corporateAction($docx, $report_id);
+	issuesOfShares($docx, $report_id);
+	alterrationInMoaAoa($docx, $report_id);
+	fillInvestmentLimits($docx, $report_id);
+	delistingOfShares($docx, $report_id);
+	donationToCharitableTrust($docx, $report_id);
+	officeOfProfit($docx, $report_id);
+	disclaimerPage($docx);
+	$com_name = getName($report_id);
+	$compnay_name=$com_name['company_name'];
+	$meeting_type=$com_name['meeting_type'];
+	$meeting_date=$com_name['meeting_date'];
+	$db_date = $meeting_date;
+	$formated_day = date_format(date_create_from_format('Y-m-d', $db_date), 'd-m-Y');
+	$docx->createDocx('try');
+	$docx = new DocxUtilities();
+	$source = 'try.docx';
+	$target = 'try2.docx';
+	$docx->watermarkDocx($source, $target, $type = 'image', $options = array('image' => 'bg.png', 'height' => 900, 'width' => 780, 'decolorate' => false));
+//$docx->watermarkDocx($source, $target, $type = 'image', $options = array('image' => 'bg.png','height'=>900,'width'=>500,'decolorate'=>false));
+	require_once '../classes/MultiMerge.inc';
+	$merge = new MultiMerge();
+	$merge->mergeDocx('index_page.docx', array('try2.docx'), "$compnay_name"."_SES Proxy Advisory Report_"."$meeting_type"."_"."$formated_day".".docx", array());
+	burnExcel($report_id);
+	$zip_files_array = array(
+		'AuditorsRemuneration.docx',
+		'AverageCommission.docx',
+		'BoardComposition.docx',
+		'CSRContribution.docx',
+		'DividendAndEarning.docx',
+		'DividendPayoutRatio.docx',
+		'EDRemuneration.docx',
+		'ExecutiveCompensation.docx',
+		'ExecutiveRemuneration.docx',
+		'MasterExcelFile.xlsx',
+		'PromoterShareholding.docx',
+		'RemunerationComponents.docx',
+		'RetireByRotation.docx',
+		'ShareholdingPattern.docx',
+		'StockPerformance.docx',
+		'StockPrice.docx',
+		'TotalCommission.docx',
+		'UtilizationBorrowingLimits.docx',
+		'VariationInDirectorsRemuneration.docx',
+		"$compnay_name"."_SES Proxy Advisory Report_"."$meeting_type"."_"."$formated_day".".docx",
+		'graph_excel.xlsx'
+	);
+	$zip = new ZipArchive;
+	if ($zip->open("$compnay_name.zip", ZipArchive::CREATE)) {
+		foreach ($zip_files_array as $zip_file_name) {
+			$zip->addFile($zip_file_name);
+		}
+	}
+	$zip->close();
+	$zip_file = "$compnay_name.zip";
+	header('Content-type: application/zip');
+	header('Content-Disposition: attachment; filename="' . basename($zip_file) . '"');
+	header("Content-length: " . filesize($zip_file));
+	header("Pragma: no-cache");
+	header("Expires: 0");
+	ob_clean();
+	flush();
+	readfile($zip_file);
+	unlink($zip_file);
+	exit;
 }
 catch(Exception $e) {
 	echo $e->getMessage();
